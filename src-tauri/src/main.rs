1928b02fbe734f64fd8ec52262bf3f0ab819eb56
Cody Krist <137073012+cmkrist@users.noreply.github.com>
2024-03-20 12:58:23 -0400

    Fix startup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -101,15 +101,14 @@ fn main() {
             println!("Not implemented yet!");
             exit(1);
         }
-        tauri::Builder::default()
-            .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-                println!("{}, {argv:?}, {cwd}", app.package_info().name);
-
-                app.emit_all("single-instance", Payload { args: argv, cwd })
-                    .unwrap();
-            }))
-            .invoke_handler(tauri::generate_handler![open_hosts, save_hosts])
-            .run(tauri::generate_context!())
-            .expect("error while running tauri application");
     });
+    tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+            app.emit_all("single-instance", Payload { args: argv, cwd })
+                .unwrap();
+        }))
+        .invoke_handler(tauri::generate_handler![open_hosts, save_hosts])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
